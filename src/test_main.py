201a6e0c31f731d622c94e3ece8ede803d70d340
Sven Ochmann <sven.och@outlook.com>
2023-11-22 06:38:48 +0100

    code fix

--- src/test_main.py
+++ src/test_main.py
@@ -1,7 +1,4 @@
-import sys 
-sys.path.append("..")
-
-from src.fair_sharer import fair_sharer
+from main import fair_sharer
 
 def test_fair_sharer():
     assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
